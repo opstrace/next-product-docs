ca58d5643b6bfd2bfa9e044b556e93c8d95e2f28
Christian Mejlak <chris@mejlak.com>
2021-08-04 17:41:46 +0200

    feat: ability to add a custom theme for codeblocks
    Signed-off-by: Christian Mejlak <chris@mejlak.com>

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import InlineCode from './components/InlineCode.jsx'
 import Tabs, { Tab } from './components/Tabs.jsx'
 import Interpolate from './components/Interpolate.jsx'
 
-export default function Documentation({ source }) {
+export default function Documentation({ source, theme }) {
   const components = {
     Tabs: Tabs,
     Tab: Tab,
